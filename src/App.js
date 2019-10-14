487e4881bf1ce9ebb95ee5d7524ad43375440a07
Ronny <ronny_the_fly7@hotmail.com>
2019-10-14 13:02:22 -0500

    probando el api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { Component, Fragment } from 'react';
 
 import './App.css';
+import { async } from 'q';
 
 class App extends Component{
 	state = {
 		url: 'https://api.github.com/users/',
 		clientId: '00244b17a2ec647fe639',
-		clientSecret: '4925ec68e9463b84e4214a271d9ed1a7af0eb063'
+		clientSecret: '4925ec68e9463b84e4214a271d9ed1a7af0eb063',
+		input:'',
+		data:null
 	}
-	async getUser(e){
-
-		const res = await fetch(`${this.state.url}${e}?client_id`);
+	getUser = async(e)=>{
+		let self = this.state;
+		fetch(`${self.url}${e}?client_id=${self.clientId}&clien_secret=${self.clientSecret}`)
+		.then((response)=>{
+			return response.json();
+		})
+		.then((json)=>{
+			this.setState({data:json});
+		});
+	}
+	handleChange = e =>{
+		let self = this.state;
+		console.log(e.target.value, self);
+		this.getUser(e.target.value);
 	}
 	componentDidMount(){
+		console.log(this.state.url)
 	}
 
 	render(){
+		console.log('ronals', this.state.data);
 		return <Fragment>
-				<input onChange={()=>this.getUser()} />
+				<input defaultValue={this.state.input} onChange={this.handleChange} />
 			</Fragment>
 	}
 }
