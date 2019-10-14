f154fa1a03d22e1baf32b2d3196ed8f072ddb95e
Ronny <ronny_the_fly7@hotmail.com>
2019-10-14 16:10:40 -0500

    se agrega la seccion de repositorios de los usuarios

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ class App extends Component{
 		clientSecret: '4925ec68e9463b84e4214a271d9ed1a7af0eb063',
 		input:'',
 		data:null,
+		dataRepo:null,
 		loading:false,
-		error:false
+		loadingRepo:false,
+		error:false,
+		errorRepo:false
 	}
 	getUser = async(e)=>{
+		let word = e;
 		let self = this.state;
-		fetch(`${self.url}${e}?client_id=${self.clientId}&clien_secret=${self.clientSecret}`)
+		fetch(`${self.url}${word}?client_id=${self.clientId}&clien_secret=${self.clientSecret}`)
 		.then((response)=>{
 			return response.json();
 		})
@@ -23,9 +27,28 @@ class App extends Component{
 			if(json.message){
 				this.setState({data:null,loading:false, error:true});
 			}else{
-				this.setState({data:json,loading:false, error:false});
+				this.setState({data:json,loading:false, loadingRepo:true, error:false});
 			}
 		});
+		this.getRepos(word);
+	}
+	getRepos = async (e)=>{
+		let self = this.state;
+		if(e.length > 0){
+			fetch(`${self.url}${e}/repos?client_id=${self.clientId}&clien_secret=${self.clientSecret}`)
+			.then((response)=>{
+				return response.json();
+			})
+			.then((json)=>{
+				if(json.message){
+					this.setState({dataRepo:null,loadingRepo:false, errorRepo:true});
+				}else{
+					this.setState({dataRepo:json,loadingRepo:false, errorRepo:false});
+				}
+			});
+		}else{
+			this.setState({dataRepo:null,loadingRepo:false, errorRepo:false});
+		}
 	}
 	handleChange = e =>{
 		this.setState({loading:true});
@@ -44,10 +67,10 @@ class App extends Component{
 					</div>
 					<div className="column is-full">
 						{this.state.error ?
-							<article class="message is-danger">
-								<div class="message-header">
+							<article className="message is-danger">
+								<div className="message-header">
 									<p>El usuario no éxiste</p>
-									<button class="delete" aria-label="delete"></button>
+									<button className="delete" aria-label="delete"></button>
 								</div>
 						  </article>:null
 						}
@@ -65,18 +88,39 @@ class App extends Component{
 										</figure>
 									</div>
 									<div className="media-content">
-										<p className="title is-4">John Smith</p>
-										<p className="subtitle is-6">@johnsmith</p>
+										<p className="title is-4">{this.state.data.name}</p>
+										<p className="subtitle is-6">@{this.state.data.login}</p>
 									</div>
 								</div>
 								<div className="content">
-								<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-								<a href="#">#css</a> <a href="#">#responsive</a>
-								</span>
+								<span>{this.state.data.location}</span>
 								</div>
 							</div>
 						</div>: this.state.loading ? <progress className="progress is-small is-primary" max="100">100%</progress>:null}
+						{this.state.dataRepo ? <nav className="panel">
+							<p className="panel-heading">
+								repositorios
+							</p>
+							<div className="panel-block">
+								<p className="control has-icons-left">
+								</p>
+							</div>
+							{this.state.dataRepo.map((ele, idx)=>{
+								return (
+									<a href="javascript:;" className="panel-block">
+										<span className="panel-icon">
+										<i className="fas fa-code-branch" aria-hidden="true"></i>
+										</span>
+										{ele.name}
+									</a>
+								)
+							})}
+						</nav>: this.state.errorRepo ?  <article className="message is-danger">
+								<div className="message-header">
+									<p>El usuario no tiene repositorios disponibles</p>
+									<button className="delete" aria-label="delete"></button>
+								</div>
+						  </article>:null}
 					</div>
 				</div>
 			</Fragment>)
