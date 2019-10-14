d541d2b0dac5f1d5577444e1462c3c0b63ebd6ed
Ronny <ronny_the_fly7@hotmail.com>
2019-10-14 14:49:19 -0500

    se agrega bulma y se maqueta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
-
 import './App.css';
-import { async } from 'q';
+
+import "../node_modules/bulma/css/bulma.css";
 
 class App extends Component{
 	state = {
@@ -9,7 +9,9 @@ class App extends Component{
 		clientId: '00244b17a2ec647fe639',
 		clientSecret: '4925ec68e9463b84e4214a271d9ed1a7af0eb063',
 		input:'',
-		data:null
+		data:null,
+		loading:false,
+		error:false
 	}
 	getUser = async(e)=>{
 		let self = this.state;
@@ -18,12 +20,15 @@ class App extends Component{
 			return response.json();
 		})
 		.then((json)=>{
-			this.setState({data:json});
+			if(json.message){
+				this.setState({data:null,loading:false, error:true});
+			}else{
+				this.setState({data:json,loading:false, error:false});
+			}
 		});
 	}
 	handleChange = e =>{
-		let self = this.state;
-		console.log(e.target.value, self);
+		this.setState({loading:true});
 		this.getUser(e.target.value);
 	}
 	componentDidMount(){
@@ -31,10 +36,50 @@ class App extends Component{
 	}
 
 	render(){
-		console.log('ronals', this.state.data);
-		return <Fragment>
-				<input defaultValue={this.state.input} onChange={this.handleChange} />
-			</Fragment>
+		return (
+			<Fragment>
+				<div className="container">
+					<div className="column is-full">
+						<input type="text" className="input" defaultValue={this.state.input} onChange={this.handleChange} placeholder="Busca tu usuario" />
+					</div>
+					<div className="column is-full">
+						{this.state.error ?
+							<article class="message is-danger">
+								<div class="message-header">
+									<p>El usuario no éxiste</p>
+									<button class="delete" aria-label="delete"></button>
+								</div>
+						  </article>:null
+						}
+						{this.state.data ? <div className="card">
+  							<div className="card-image">
+							  	<figure className="image is-4by3">
+									<img src={this.state.data.avatar_url} alt="Placeholder image" />
+								</figure>
+							</div>
+							<div className="card-content">
+								<div className="media">
+									<div className="media-left">
+										<figure className="image is-48x48">
+											<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+										</figure>
+									</div>
+									<div className="media-content">
+										<p className="title is-4">John Smith</p>
+										<p className="subtitle is-6">@johnsmith</p>
+									</div>
+								</div>
+								<div className="content">
+								<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+								<a href="#">#css</a> <a href="#">#responsive</a>
+								</span>
+								</div>
+							</div>
+						</div>: this.state.loading ? <progress className="progress is-small is-primary" max="100">100%</progress>:null}
+					</div>
+				</div>
+			</Fragment>)
 	}
 }
 
